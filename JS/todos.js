2678dda4f10a9cbc9724b6d214fb74157f31fdbe
Amaris Sanders <146863945+AmarisNichole@users.noreply.github.com>
2023-12-10 18:58:26 -0500

    modified files and added an image

--- JS/todos.js
+++ JS/todos.js
@@ -1,6 +1,6 @@
 window.onload = () => {
   let userDropdown = document.getElementById("user-dropdown");
-  let userIdSubmit = document.getElementById("user-id-submit");
+  let viewTasksBtn = document.getElementById("user-id-submit");
   let tasksList = document.getElementById("tasks-list");
 
   // Fetch user data and populate the dropdown
@@ -25,64 +25,25 @@ window.onload = () => {
   // Fetch and display task when the user clicks the button
 
   viewTasksBtn.onclick = () => {
+    // Use userDropdown.value to get the selected value
     let selectedUserId = userDropdown.value;
 
-    if (selectedUserId) {
-      fetch(`http://localhost:8083/api/todos/byuser/${selectedUserId}`)
-        .then((res) => {
-          return res.json;
-        })
-        .then((userTasks) => {
-          tasksList.innerHTML = "";
-
-          userTasks.forEach((task) => {
-            let li = document.createElement("li");
-            li.innerHTML = `
-                            
-            <p>Category: ${todo.category}</p>
-            <p>Description: ${todo.description}</p>
-            <p>Deadline: ${todo.deadline}</p>
-            <p>Priority: ${todo.priority}</p>
-            <p>Completed: ${todo.completed ? "Yes" : "No"}</p>
+    // Fetch ToDo details for the selected user
+    fetch(`http://localhost:8083/api/todos/byuser/${selectedUserId}`)
+        .then((res) => res.json())
+        .then((task) => {
+            let userDetails = document.getElementById("user-details");
+            
+            userDetails.innerHTML = `
+                <h3>User ToDo Details</h3>
+                <p>Category: ${task.category}</p>
+                <p>Description: ${task.description}</p>
+                <p>Deadline: ${task.deadline}</p>
+                <p>Priority: ${task.priority}</p>
+                <p>Completed: ${task.completed ? 'Yes' : 'No'}</p>
             `;
-          });
-        });
-    }
+        })
+        .catch(error => console.error('Error fetching ToDo details:', error));
+};
   };
 
-  // let submitBtn = document.getElementById("user-id-submit");
-  // let userDropdown = document.getElementById("user-dropdown");
-
-  // fetch("http://localhost:8083/api/todos")
-  //     .then((res) => res.json())
-  //     .then((todos) => {
-  //         for (let todo of todos) {
-  //             // Convert userid to a string for consistency
-  //             let userIdString = todo.userid.toString();
-
-  //             let optionEl = new Option(userIdString);
-  //             userDropdown.appendChild(optionEl);
-  //         }
-  //     });
-
-  // submitBtn.onclick = () => {
-  //     // Use userDropdown.value to get the selected value
-  //     let selectedUserId = userDropdown.value;
-
-  //     // Fetch ToDo details for the selected user
-  //     fetch(`http://localhost:8083/api/todos/${selectedUserId}`)
-  //         .then((res) => res.json())
-  //         .then((todo) => {
-  //             let userDetails = document.getElementById("user-details");
-  //             userDetails.innerHTML = `
-  //                 <h3>User ToDo Details</h3>
-  //                 <p>Category: ${todo.category}</p>
-  //                 <p>Description: ${todo.description}</p>
-  //                 <p>Deadline: ${todo.deadline}</p>
-  //                 <p>Priority: ${todo.priority}</p>
-  //                 <p>Completed: ${todo.completed ? 'Yes' : 'No'}</p>
-  //             `;
-  //         })
-  //         .catch(error => console.error('Error fetching ToDo details:', error));
-  // };
-};
